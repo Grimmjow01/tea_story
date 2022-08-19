0c4a03f5129359767defc02b702406b055c84146
Marina Shopina <marinashopina95@gmail.com>
2022-08-19 13:13:39 +0300

    uzhe pochti vsyo

--- src/views/ShowTea.jsx
+++ src/views/ShowTea.jsx
@@ -7,26 +7,28 @@ const Layout = require('./Layout');
 module.exports = function ShowTea({ oneTea, newUser }) {
   return (
     <Layout newUser={newUser}>
-      <script defer src="/js/deleteTea.js"/>
-      <div className="edit card card-body show-one-tea-div-flex">
-        <div>
-          <img src={oneTea.image_url} alt="image error" width="150px" height="150px" />
-        </div>
-        <div className="width250">
-          <h1>{oneTea.title}</h1>
-          <span className="entry-date block font-3-4 c-lt-gray margin5 font-small">
-            Written on
-            {' '}
-            {String(oneTea.createdAt).slice(0, 16)}
-          </span>
-          <p>{oneTea.sort_tea}</p>
-          <div className="btn-show-one-tea-div-flex">
-            <a href={`/profile/${oneTea.id}/edit`} className="teaEdit btn btn-success">Редактировать</a>
-            <button id="deleteTeaButton" data-onteaid={oneTea.id} name="deleteTeaButton" className="btn btn-success">
-              Удалить
-            </button>
-          </div>
-          {/* <ul id="editAndDeleteUl" className="no-bullets">
+      <div className="show-one-tea">
+        <script defer src="/js/deleteTea.js" />
+        <div className="flex-div-1">
+          <div className="edit card card-body show-one-tea-div-flex">
+            <div>
+              <img src={oneTea.image_url} alt="image error" width="150px" height="150px" />
+            </div>
+            <div className="width250">
+              <h1>{oneTea.title}</h1>
+              <span className="entry-date block font-3-4 c-lt-gray margin5 font-small">
+                Written on
+                {' '}
+                {String(oneTea.createdAt).slice(0, 16)}
+              </span>
+              <p>{oneTea.sort_tea}</p>
+              <div className="btn-show-one-tea-div-flex">
+                <a href={`/profile/${oneTea.id}/edit`} className="teaEdit btn btn-success">Редактировать</a>
+                <button id="deleteTeaButton" data-onteaid={oneTea.id} name="deleteTeaButton" className="btn btn-success">
+                  Удалить
+                </button>
+              </div>
+              {/* <ul id="editAndDeleteUl" className="no-bullets">
             <li className="pipe-separate left">
               <a href={`/profile/${oneTea.id}/edit`} className="teaEdit btn btn-success">Редактировать</a>
             </li>
@@ -36,6 +38,8 @@ module.exports = function ShowTea({ oneTea, newUser }) {
               </button>
             </li>
           </ul> */}
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
